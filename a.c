2506ebb399f5df6609b232e400493e340a81bc6b
Soniq Nagra <soniq.nagra@uon.edu.au>
2019-04-03 07:46:40 +0000

    Practicing using pointers.  I tried to use pointers with char and arrays. Its not working very well. So I'm going to back to basics and use pointers with just int.

--- a.c
+++ a.c
@@ -2,11 +2,16 @@
 
 int main() {
 
-    char a[10];
+    char a[5] = {'h','e','l','l','o'};
+    char *ch;
+    
+    *ch = &a[5];
     
     scanf("%s", &a);
     
     printf ("%s \n", a);
+    
+    printf ("%s \n", *ch);
   
   return 0;
   
