1f33e12edbc8f8f9dfb6ed460c8f3c1e8ac1b22c
Soniq Nagra <soniq.nagra@uon.edu.au>
2019-04-15 03:02:56 +0000

    This only gets one character coverts into  ASCII and changes and returns it to terminal

---
+++ LCS.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    char x;
+    int c;
+    
+    scanf("%c", &x);
+    
+    c = x;
+    
+    for ( c; 97 < c < 127; c++){}
+    
+    x = c;
+    
+    printf("%c\n", x);
+    
+   /* for (int i = 0; i <1023; i++) {
+    
+    if (x[i] = 'a') {
+        'a' = 'b';
+        
+    } else if (x[i] = 'b') {
+        'b' = 'c';
+        
+    } else if (x[i] = 'c') {
+        'c' = 'd';
+        
+    } else if (x[i] = 'd') {
+        'd' = 'e';
+    
+    } else if (x[i] = 'e') {
+        'e' = 'f';
+    
+    } else if (x[i] = 'f') {
+        'f' = 'g';
+    
+    } else if (x[i] = 'g') {
+        'g' = 'h';
+    
+    } else if (x[i] = 'h') {
+        'h' = 'i';
+    
+    } else if (x[i] = 'i') {
+        'i' = 'j';
+    
+    } else if (x[i] = 'j') {
+        'j' = 'k';
+    
+    } else if (x[i] = 'k') {
+        'k' = 'l';
+    
+    } else if (x[i] = 'l') {
+        'l' = 'm';
+    
+    } else if (x[i] = 'm') {
+        'm' = 'n';
+    
+    } else if (x[i] = 'n') {
+        'n' = 'o';
+    
+    } else if (x[i] = 'o') {
+        'o' = 'p';
+    
+    } else if (x[i] = 'p') {
+        'p' = 'q';
+    
+    } else if (x[i] = 'q') {
+        'q' = 'r';
+    
+    } else if (x[i] = 'r') {
+        'r' = 's';
+    
+    } else if (x[i] = 's') {
+        's' = 't';
+    
+    } else if (x[i] = 't') {
+        't' = 'u';
+    
+    } else if (x[i] = 'u') {
+        'u' = 'v';
+    
+    } else if (x[i] = 'v') {
+        'v' = 'w';
+    
+    } else if (x[i] = 'w') {
+        'w' = 'x';
+    
+    } else if (x[i] = 'x') {
+        'x' = 'y';
+    
+    } else if (x[i] = 'y') {
+        'y' = 'z';
+    
+    } else if (x[i] = 'z') {
+        'z' = 'a';
+    
+    }
+
+} */
+   // printf("%s", x);
+}
