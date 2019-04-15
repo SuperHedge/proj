1b3e6bc21308113346d5f1622c5ba5cafa37b4e7
Soniq Nagra <soniq.nagra@uon.edu.au>
2019-04-15 07:14:16 +0000

    After doing some research I found how to write the ceaser cipher code. Now I need to develop a code without looking at that site again, eitherwise I'll just be plagirising.

---
+++ RCC.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    char c[1024], ch;
+    int x, i;
+    
+    printf("Enter : ");
+    gets(c);
+    
+    for (i = 0; c[i] != '\0'; i++)
+    
+    ch = c[i];
+    
+ 
+}
