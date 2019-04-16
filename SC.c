c40b208cfa185d61d184624f41a81385611943fe
Soniq Nagra <soniq.nagra@uon.edu.au>
2019-04-16 03:36:24 +0000

    This code that I found only covers lower case letters only, so now I need to replicate this code and do upper case.
    Layout
    
    Function Lower
    
    Function Upper
    
    Main
    
           if Lower go to function lower
    
           else if go to function upper
    
    print

--- SC.c
+++ SC.c
@@ -1,130 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    
-    char ch[1024];
-    int x;
-    
-    printf("Enter : ");
-    gets(x);
-    
-    for (int i = 0; ch[i] != '\0'; i++) {
-        
-        x = ch[i];
-        
-        if (x = 97){
-	
-	       x = 113; }
-	
-       /* else if (x = 'b'){
-	
-	       x = 'w'; }
-	
-        else if (x = 'c'){
-	
-	       x = 'e'; }
-	
-        else if (x = 'd'){
-	
-	       x = 'r'; }
-	
-        else if (x = 'e'){
-	
-	       x = 't'; }
-	
-        else if (x = 'f'){
-	
-	       x = 'y'; }
-	
-        if (x = 'g'){
-	
-	       x = 'u'; }
-	
-        if (x = 'h'){
-	
-	       x = 'i'; }
-	
-        if (x = 'i'){
-	
-	       x = 'o'; }
-	
-        if (x = 'j'){
-	
-	       x = 'p'; }
-	
-        if (x = 'k'){
-	
-	       x = 'a'; }
-	
-        if (x = 'l'){
-	
-	       x = 's'; }
-	
-        if (x = 'm'){
-	
-	       x = 'd'; }
-	
-        if (x = 'n'){
-	
-	       x = 'f'; }
-	
-        if (x = 'o'){
-	
-	       x = 'g'; }
-	   
-        if (x = 'p'){
-	
-	       x = 'h'; }
-	
-        if (x = 'q'){
-	
-	       x = 'j'; }
-	
-        if (x = 'r'){
-	
-	       x = 'k'; }
-	
-        if (x = 's'){
-	
-	       x = 'l'; }
-	
-        if (x = 't'){
-	
-	       x = 'z'; }
-	
-        if (x = 'u'){
-	
-	       x = 'x'; }
-	
-        if (x = 'v'){
-	
-	       x = 'c'; }
-	
-        if (x = 'w'){
-	
-	       x = 'v'; }	
-	
-        if (x = 'x'){
-	
-	       x = 'b'; }
-	
-        if (x = 'y'){
-	
-	       x = 'n'; }
-	
-        if (x = 'z'){
-	
-	       x = 'm'; }*/
-            
-        
-        ch[i] = x;
-        
-        printf("%c", ch[i]);
-        
+int () {
+    char* ciphertext = "qwertyuiopasdfghjklzxcvbnm";    // cipher lookup
+
+    char input[500];                                    // input buffer
+    printf("Enter text: ");
+    fgets(input, sizeof(input), stdin);                 // safe input from user
+    input[strlen(input) - 1] = 0;                       // remove the \n (newline)
+    int count = strlen(input);                          // get the string length
+
+    char output[count];                                 // output string
+    for(int i = 0; i < count; i++) {                    // loop through characters in input
+        int index = ((int) input[i]) - 97;              // get the index in the cipher by subtracting 'a' (97) from the current character
+        if(index < 0) {
+            output[i] = ' ';                            // if index < 0, put a space to account for spaces
+        }
+        else {
+            output[i] = ciphertext[index];              // else, assign the output[i] to the ciphertext[index]
+        }
     }
-    
-    printf("\n");
+    output[count] = 0;                                  // null-terminate the string
 
-    return 0;
-    
+    printf("output: %s\n", output);                     // output the result
+}
+
+int capital() {
+    char* ciphertext = "QWERTYUIOPASDFGHJKLZXCVBNM";    // cipher lookup                          
+                                                        
+    char output[count];                                 // output string
+    for(int i = 0; i < count; i++) {                    // loop through characters in input
+        int index = ((int) input[i]) - 65;              // get the index in the cipher by subtracting 'a' (97) from the current character
+        if(index < 0) {
+            output[i] = ' ';                            // if index < 0, put a space to account for spaces
+        }
+        else {
+            output[i] = ciphertext[index];              // else, assign the output[i] to the ciphertext[index]
+        }
+    }
+    output[count] = 0;                                  // null-terminate the string
+
+    printf("output: %s\n", output);                     // output the result
 }
