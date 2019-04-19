7715c34d1cb79ad546c6fbcf423a1c8f2d3c046a
Soniq Nagra <soniq.nagra@uon.edu.au>
2019-04-19 01:38:53 +0000

    Code is not working now. So now I'm going to revert it back to when it was working

--- SC.c
+++ SC.c
@@ -1,67 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 
-int lower() {
-    char* ciphertext = "qwertyuiopasdfghjklzxcvbnm";                   
-    
-    for(int i = 0; i < count; i++) {
-        
-        int index = ((int) input[i]) - 97;   
-        
-        if(index < 0) {
-            
-            output[i] = ' ';             
-            
-        }
-        
-        else {
-            
-            output[i] = ciphertext[index];           
-        }
-    }
-    
-    output[count] = 0;                                    
-    
-}
-
-int upper() {
-    char* Uciphertext = "QWERTYUIOPASDFGHJKLZXCVBNM";                
-    
-    for(int i = 0; i < count; i++) {
-        
-        int Index = ((int) ch[i]) - 65;   
-        
-        if (Index < 0) {
-            Output[j] = ' ';        
-            
-        }
-        
-        else {
-            Output[i] = Uciphertext[index];        
-        }
-    }
-    
-    output[count] = 0;                                        
-    
-}
-
 int main() {
     
-    char ch[500];  
+    char* ciphertext = "qwertyuiopasdfghjklzxcvbnm";
+    char* Uciphertext = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    
+    char ch[1024];  
     
     printf("Enter text: ");
+    fgets(ch, sizeof(ch), stdin);                            
+    ch[strlen(ch) - 1] = 0;                       
+    int count = strlen(ch);
     
-    fgets(ch, sizeof(ch), stdin);
-    
-    ch[strlen(ch) - 1] = 0;       
-    
-    int count = strlen(ch);                          
+    for (int i = 0; ch[i] != '\0'; i++) {
+        
+        int x = ch[i];
+        
+        if (x > 65 && x < 90) {
+            
+            char output[i];                                 
+                                
+                int index = ((int) ch[i]) - 65;             
+                if(index < 0) {
+                    output[i] = ' ';                            
+                }
+                else {
+                     output[i] = Uciphertext[index];              
+               }
 
-
-    char output[count];
-    
-    for (i = 0; ch[i] != '\0'; i++) {
+            output[i] = 0; 
+            
+            printf("%s", output);
+        }
         
         
+        else if (x > 97 && x < 122) {
+            
+            char output[i];         
+
+                int index = ((int) ch[i]) - 97;   
+        
+                if (index < 0) {
+                    
+                    output[i] = ' ';        
+            
+                } else {
+                     
+                    output[i] = ciphertext[index];        
+                }
+
+    
+            output[i] = 0;
+            
+            printf("%s", output);
+        }
+        
+
     }
+    
+    printf("\n");                                   
+    
 }
