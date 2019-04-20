081c167d03e36b2ceae4e8f6c4c1b31b4e4ee3c4
Soniq Nagra <soniq.nagra@uon.edu.au>
2019-04-20 10:00:33 +0000

    What I've done is put an interface where you can choose to input a file or input a sentence

--- FileIORCC.c
+++ FileIORCC.c
@@ -5,29 +5,29 @@ int main(void) {
     char c[1024], A;
     int x, i, mod;
     
+     printf("Cipher a file (a)\n\n");
+     printf("Cipher a sentence (b)\n\n");
+        
     do {
-        printf("Cipher a file (a)\n\n");
-        printf("Cipher a sentence (b)\n\n");
         scanf("%c", &A);
     } while (A != 'a' && A != 'b');
     
     if (A = 'b') {
-        char file;
+        char file[1024];
         
         printf("Enter file name : ");
-        scanf("%s\n", file);
+        gets(file);
         
         FILE *fopen(const char *file, const char *r);
     }
     else if (A = 'b') {
-    printf("Enter Sentence to Cipher: ");
-    gets(c);
-    printf("\n");
+        printf("Enter Sentence to Cipher: ");
+        gets(c);
+        printf("\n");
     }
 
-    
+    printf("Enter Shift : ");
     do {
-        printf("Enter Shift : ");
         scanf("%d", &mod);
     } while (mod < 1024 && mod > 1024);
 
