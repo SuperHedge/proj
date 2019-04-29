5cc904c9f5516a414b1e478de66422694635aa35
Soniq Nagra <soniq.nagra@uon.edu.au>
2019-04-29 04:49:25 +0000

    Work in progress

--- UI.c
+++ UI.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-char RCC (char k[1024]);
-char RCD (char c[1024]);
+char RCC (char *v[]);
+char RCD (int *v[]);
 
 
 int main () {
     
-    char q, v;
+    char q, k[1024];
+    
+    printf("Enter message : ");
+    gets();
     
     printf("Enter a for rotation cipher \nEnter b for rotation decipher \nEnter c for substitution cipher \n");
     
@@ -15,34 +18,30 @@ int main () {
     }while (q != 'a' && q != 'b' && q != 'c');
     
     if (q == 'a') {
-        RCC;
+        RCC(k[1024]);
         
     } else if (q == 'b') {
-        RCD;
+        RCD(k[1024]);
         
     } else if (q == 'c') {
-        printf("=P");
+        printf("=\nP");
     }
     
     return 0;
 }
 
-char RCC(char k[1024]) {
-                     //declaring some variables
-    int y, j, Mod;
-    
-    printf("Enter Sentence to Cipher: ");
-    gets(k);                                //Using gets() allows for white space to be read
-    
+char RCC(char *v) {
+     
+    int j, Mod;                          //declaring some variables
+
     printf("Enter Positive Shift : ");
     scanf("%d", &Mod);
-
     
     printf("Ciphered code is : ");          // this printf goes here eitherwise after each letter printed would have this behind it.
     
-    for (j = 0; k[j] != '\0'; j++) {        // this 'for' loop is to cycle through each character given.
+    for (j = 0; v[j] != '\0'; j++) {        // this 'for' loop is to cycle through each character given.
     
-        y = k[j];                           // convet the character into a number
+        y = v[j];                           // convet the character into a number
         
         if (y <= 'z' && y >= 'a') {         // if the character is lowercase it goes through this 'if' statement
             
@@ -63,62 +62,63 @@ char RCC(char k[1024]) {
         
         }
         
-    k[j] = y;                               // converts the number back to ASCII
+    v[j] = y;                               // converts the number back to ASCII
     
-    printf("%c", k[j]);                     // Then prints out the result
+    printf("%c", v[j]);                     // Then prints out the result
     
     }
     
     printf("\n");                           // I couldn't include this in the loop because each letter would print on a different line.
     
-    return y;
+    return Mod;
 
 }
 
-char RCD(char c[1024]) {                   // Basically the same as the cipher, except word changes and the mod is a minus
+char RCD(int x) {                   // Basically the same as the cipher, except word changes and the mod is a minus
     
-    int x, i, mod;
+    char v[1024];
+    int i, mod;
     
-    printf("Enter Sentence to Decipher: ");
-    gets(c);
-    
-    do {
-        printf("Enter Shift : ");
-        scanf("%d", &mod);
-    } while (mod < 26 && mod > 0);
+    printf("Enter message : ");
+    gets(v);
     
     printf("Diphered code is : ");
     
-    for (i = 0; c[i] != '\0'; i++) {
-    
-        x = c[i];
+    for (int rot = 0; rot <= 26; rot++) {
         
-        if (x <= 'z' && x >= 'a') {
+        for (i = 0; v[i] != '\0'; i++) {
+    
+            x = v[i];
+        
+            if (x <= 'z' && x >= 'a') {
             
-            x = x - mod;
+                x = x - mod;
             
-        } else if (x <= 'Z' && x >= 'A') {
+            } else if (x <= 'Z' && x >= 'A') {
             
-            x = x - mod;
+                x = x - mod;
             
+            }
+            if (x == '@') {
+            
+                x = 'Z';
+        
+            } else if (x == '`') {
+            
+                x = 'z';
+        
+            }
+        
+        v[i] = x;
+    
+        printf("%c", v[i]);
+    
         }
-        if (x == '@') {
-            
-            x = 'Z';
-        
-        } else if (x == '`') {
-            
-            x = 'z';
-        
-        }
-        
-    c[i] = x;
-    
-    printf("%c", c[i]);
-    
-    }
     
     printf("\n");
     
-    return x;
+    }
+
+
+    return mod;
 }
